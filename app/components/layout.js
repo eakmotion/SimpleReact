6e3e4a9774b8e6d46f2b726f611f2c142ba5c393
eakmotion <eakpun@gmail.com>
2017-02-08 19:10:07 +0700

    Setup PureCSS

--- app/components/layout.js
+++ app/components/layout.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Layout extends React.Component {
   render() {
     return (
-      <div id='Layout'>
-        Hello from Layout!
+      <div id='Layout' className='pure-g'>
+        <div className="pure-u-1-3"><p>First</p></div>
+        <div className="pure-u-1-3"><p>Second</p></div>
+        <div className="pure-u-1-3"><p>Third</p></div>
       </div>
     );
   }
